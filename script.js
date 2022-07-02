9857302f68b454b429642105b681b90cb6f41526
Josue Fernandes <josue7mf@hotmail.com>
2022-07-02 20:45:05 -0300

    checkbox personalizada

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ const input = document.getElementById('input'); // cadastro
 const submit = document.getElementById('submit'); // adicionar
 const remove = document.getElementById('remove'); // remover
 let resultado = document.getElementById('resultado');
-console.log(input,submit,remove);
 
 let itens;
 let marcado;
 
 function addItem() {
     if(input.value != '') {
-        resultado.innerHTML += `<p>${input.value}<input type="checkbox" class="Visible"></p>`;
+        resultado.innerHTML += `<p style="">${input.value}<input type="checkbox" class="Visible"></p>`;
+        let p = document.querySelectorAll('#resultado p');
+        let check = document.querySelectorAll('#resultado p input[type=checkbox]');
+        itens = Array.from(p);
+        marcado = Array.from(check);
+    } else {
+        alert('Item Inválido, tente novamente');
     }
-    let p = document.querySelectorAll('#resultado p');
-    let check = document.querySelectorAll('#resultado p input[type=checkbox]');
-    itens = Array.from(p);
-    marcado = Array.from(check);
 }
 
 submit.addEventListener('click', addItem);
@@ -27,4 +28,5 @@ function removeItem() {
             item.parentNode.remove();
         } 
     })
+
 }
