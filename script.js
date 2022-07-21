d117c81defe676fa388889a15491095d2237a8f4
Josue Fernandes <josue7mf@hotmail.com>
2022-07-20 22:11:51 -0300

    removeAll

--- script.js
+++ script.js
@@ -2,31 +2,46 @@
 const input = document.getElementById('input'); // cadastro
 const submit = document.getElementById('submit'); // adicionar
 const remove = document.getElementById('remove'); // remover
+const removeAll = document.getElementById('removeAll'); // remover
 let resultado = document.getElementById('resultado');
 
 let itens;
-let marcado;
+let checkBox = [];
+
+submit.addEventListener('click', addItem);
+remove.addEventListener('click', removeItem);
+removeAll.addEventListener('click', removeAllItems);
+
+
 
 function addItem() {
     if(input.value != '') {
         resultado.innerHTML += `<p style="">${input.value}<input type="checkbox" class="Visible"></p>`;
-        let p = document.querySelectorAll('#resultado p');
-        let check = document.querySelectorAll('#resultado p input[type=checkbox]');
-        itens = Array.from(p);
-        marcado = Array.from(check);
+        checkBox = atualizaArray('input[type=checkbox]');
+        itens = atualizaArray('#resultado p');
     } else {
         alert('Item Inválido, tente novamente');
     }
 }
 
-submit.addEventListener('click', addItem);
-
-remove.addEventListener('click', removeItem);
 function removeItem() {
-    marcado.forEach(item => {
-        if(item.checked && item != undefined){
+    checkBox.forEach(item => {
+        if(item.checked){
             item.parentNode.remove();
         } 
     })
+    checkBox = atualizaArray('input[type=checkbox]');
+    itens = atualizaArray('#resultado p');
+}
+function removeAllItems() {
+    checkBox.forEach(item => item.parentNode.remove());
+}
 
+function checkItem(e) {
+    if(e.target.checked) {
+        console.log('checkado')
+    }
+}
+function atualizaArray(nodeList) {
+    return Array.from(document.querySelectorAll(nodeList));
 }
